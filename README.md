1bf25b37f7c699d6ce694d40b307521830c623bf
Darlan Alves <me@darlanalv.es>
2020-08-17 10:32:39 +0200

    feat: add random function

---
+++ README.md
@@ -0,0 +1,11 @@
+# fn random
+
+```bash
+# get a random byte
+fn random
+
+# get 354 random bytes
+fn random 354
+```
+
+Note: maximum of 4096 bytes.
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import { lambda } from '@node-lambdas/core';
+import { randomBytes } from 'crypto';
 
 const configutation = {
   version: 1,
 };
 
-lambda(configutation, (input, output) => input.pipe(output));
+lambda(configutation, (input, output) => {
+  const inputSize = input.url === '/' ? 1 : Number(input.url.slice(1)) | 0;
+  const size = inputSize ? Math.min(inputSize, 4096) : 1;
+  const buffer = randomBytes(size);
+
+  output.send(buffer);
+});
