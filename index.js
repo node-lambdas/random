5d4e45b17dee977e6b95740e37af10c5420737e8
Darlan Alves <me@darlanalv.es>
2020-08-23 00:40:21 +0200

    feat: update to v2

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import { lambda } from '@node-lambdas/core';
 import { randomBytes } from 'crypto';
 
 const configutation = {
-  version: 1,
+  version: 2,
+  actions: {
+    default: {
+      handler: (input, output) => {
+        const inputSize = input.options.size | 0;
+        const size = (inputSize && Math.min(inputSize, 4096)) || 1;
+        const buffer = randomBytes(size);
+
+        output.send(buffer);
+      },
+    },
+  },
 };
 
-lambda(configutation, (input, output) => {
-  const inputSize = input.url === '/' ? 1 : Number(input.url.slice(1)) | 0;
-  const size = inputSize ? Math.min(inputSize, 4096) : 1;
-  const buffer = randomBytes(size);
-
-  output.send(buffer);
-});
+lambda(configutation);
